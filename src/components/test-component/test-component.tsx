7af4e5992e5ae55ea2d95c4892e5ba7fa118be50
irtesam19 <mahmud@cse.uiu.ac.bd>
2024-03-05 10:23:47 +0600

    digital tosbi implemented

--- src/components/test-component/test-component.tsx
+++ src/components/test-component/test-component.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import './test-component.css'
 
 interface ITestProps{
     name: string
     age: number
 }
 
-const TestComponent = (props:ITestProps) =>{
-    const age = 50;
+const TestComponent = () =>{
+    // let count = 5;
+    const [count, setCount] = useState(0);
     
+    const increaseHandler = ()=>{
+        setCount(count + 1)
+        console.log(count)
+    }
+
+    const decreaseHandler = () =>{
+        setCount(count - 1)
+        console.log(count)
+    }
     return(
         <div>
-            Hello I am {props.name}!
-            <p>
-                My age is {props.age}
-            </p>
+            <div className="count-text">{count}</div>
+            <div className="button-container">
+                <button onClick={increaseHandler}>INCREASE</button>
+                <button onClick={decreaseHandler}>DECREASE</button>
+            </div>
         </div>
     )
 }
