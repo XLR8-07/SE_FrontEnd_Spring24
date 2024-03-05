a1770a56d9f51e17baf38313b866f16da06ee7cd
irtesam19 <mahmud@cse.uiu.ac.bd>
2024-03-05 10:24:07 +0600

    state variable introduced

--- src/components/auth-component/auth-component.tsx
+++ src/components/auth-component/auth-component.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import './auth-component.css'
 import logo from "../../assets/public/uiu_logo.png";
 
 
 const AuthComponent = ()=>{
+    const [userEmail, setUserEmail] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+
+    const submitHandler = () =>{
+        console.log(userEmail)
+        console.log(userPassword)
+    }
+
+    const handleEmailChange = (event: any)=>{
+        setUserEmail(event.target.value)
+    }
+
+    const handlePassChange = (event: any)=>{
+        setUserPassword(event.target.value)
+    }
+
     return(
         <div className="container">
             <img src={logo} alt="UIU Logo" className="image"/>
             <div className="input-container">
-                <input placeholder="email" className="text-input"/>
-                <input placeholder="password" className="pass-input" type="password"/>
-                <button className="submit-button"> SUBMIT </button>
+                <input placeholder="email" value={userEmail} onChange={handleEmailChange} className="text-input"/>
+                <input placeholder="password" value={userPassword} onChange={handlePassChange} className="pass-input" type="password"/>
+                <button className="submit-button" onClick={submitHandler}> SUBMIT </button>
             </div>
         </div>
     )
